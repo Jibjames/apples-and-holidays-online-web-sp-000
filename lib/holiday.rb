532076cb91710a9dff1d939ad2ba2034db300e8a
James Bridges <masterbridges@gmail.com>
2019-12-31 20:50:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supply|
       if holiday.to_s.include?("_")
         holiday = holiday.to_s.split("_")
-        holiday.map {|i| i.capitalize!}
+        holiday = holiday.map {|i| i.capitalize!}
         holiday = holiday.join(" ")
       end
       puts "  #{holiday.to_s.capitalize}: #{supply.join(", ")}"
