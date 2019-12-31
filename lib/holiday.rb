0a43780815db462a9ab5ff59245a9d9ea1cf1dd6
James Bridges <masterbridges@gmail.com>
2019-12-31 20:36:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supply|
-      puts "#{supply}"
+      puts "#{holiday}"
     end
   end
 end
